b109cfae50457984386c068627a1cb94504de888
Dudde420 <urtans.didzis@gmail.com>
2025-01-17 21:42:26 +0100

    first picture created

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use glam::{f64, DVec3};
 use itertools::Itertools;
+use structs::Ray;
 mod structs;
+use std::{fs, io};
 const MAX_VALUE: u8 = 255;
 const ASPECT_RATIO: f64 = 16. / 9.;
 const IMAGE_WIDTH: u32 = 400;
@@ -11,7 +13,7 @@ const FOCAL_LENGTH: f64 = 1.0;
 const CAMERA_CENTER: DVec3 = DVec3::ZERO;
 const VIEWPORT_U: DVec3 = DVec3::new(VIEWPORT_WIDTH, 0., 0.);
 const VIEWPORT_V: DVec3 = DVec3::new(0., -VIEWPORT_HEIGHT, 0.);
-fn main() {
+fn main() -> io::Result<()> {
     let pixel_delta_u: DVec3 = VIEWPORT_U / IMAGE_WIDTH as f64;
     let pixel_delta_v: DVec3 = VIEWPORT_V / IMAGE_HEIGHT as f64;
     let viewport_upper_left: DVec3 =
@@ -19,11 +21,30 @@ fn main() {
     let viewport_upper_left: DVec3 =
         CAMERA_CENTER - DVec3::new(0., 0., FOCAL_LENGTH) - VIEWPORT_U / 2. - VIEWPORT_V / 2.;
     let pixel00_loc: DVec3 = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    let mut pixels = String::new();
     for j in 0..IMAGE_HEIGHT {
         for i in 0..IMAGE_WIDTH {
             let pixel_center =
                 pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
             let ray_direction = pixel_center - CAMERA_CENTER;
+            let ray = Ray {
+                origin: CAMERA_CENTER,
+                direction: ray_direction,
+            };
+            let pixel_color = ray.color() * 255.;
+            pixels += &format!("{} {} {} \n", pixel_color.x, pixel_color.y, pixel_color.z);
         }
     }
+    fs::write(
+        "output0",
+        format!(
+            "P3
+{IMAGE_WIDTH} {IMAGE_HEIGHT}
+{MAX_VALUE}
+{pixels}
+"
+        ),
+    )?;
+
+    Ok(())
 }
--- src/structs.rs
+++ src/structs.rs
@@ -4,10 +4,10 @@ pub struct Ray {
     pub direction: DVec3,
 }
 impl Ray {
-    fn at(&self, t: f64) -> DVec3 {
+    pub fn at(&self, t: f64) -> DVec3 {
         self.origin + t * self.direction
     }
-    fn color(&self) -> DVec3 {
+    pub fn color(&self) -> DVec3 {
         let unit_direction: DVec3 = self.direction.normalize();
         let a = 0.5 * (unit_direction.y + 1.);
         return (1. - a) * DVec3::new(1., 1., 1.) + a * DVec3::new(0.5, 0.7, 1.);
