042c95726d491d70aa3b70145952826c40fd5c00
Dudde420 <urtans.didzis@gmail.com>
2025-01-19 23:37:54 +0100

    deleted old image files and added bad anti-aliasing

---
+++ src/camera.rs
@@ -0,0 +1,83 @@
+use crate::structs::*;
+use glam::DVec3;
+use rand::prelude::*;
+use std::{fs, io};
+pub struct Camera {
+    image_width: u32,
+    image_height: u32,
+    max_value: u16,
+    aspect_ratio: f64,
+    center: DVec3,
+    pixel_delta_u: DVec3,
+    pixel_delta_v: DVec3,
+    samples_per_pixel: u32,
+    pixel00_loc: DVec3,
+}
+impl Camera {
+    pub fn new(image_width: u32, aspect_ratio: f64, max_value: u16) -> Self {
+        let focal_length: f64 = 1.;
+        let viewport_height: f64 = 2.;
+        let center: DVec3 = DVec3::ZERO;
+        let image_height: u32 = (image_width as f64 / aspect_ratio) as u32;
+        let viewport_width: f64 = viewport_height * (image_width as f64 / image_height as f64);
+        let viewport_u: DVec3 = DVec3::new(viewport_width, 0., 0.);
+        let viewport_v: DVec3 = DVec3::new(0., -viewport_height, 0.);
+        let viewport_upper_left: DVec3 =
+            center - DVec3::new(0., 0., focal_length) - viewport_u / 2. - viewport_v / 2.;
+        let pixel_delta_u = viewport_u / image_width as f64;
+        let pixel_delta_v = viewport_v / image_height as f64;
+        Camera {
+            image_width,
+            image_height: (image_width as f64 / aspect_ratio) as u32,
+            center: DVec3::ZERO,
+            aspect_ratio,
+            max_value,
+            pixel_delta_u,
+            pixel_delta_v,
+            pixel00_loc: viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v),
+            samples_per_pixel: 420,
+        }
+    }
+    pub fn get_ray(&self, i: u32, j: u32) -> Ray {
+        let pixel_center =
+            self.pixel00_loc + (i as f64 * self.pixel_delta_u) + (j as f64 * self.pixel_delta_v);
+        let t1 = thread_rng().gen::<f64>() - 0.5;
+        let sample_square: DVec3 = (t1 * self.pixel_delta_u) + (t1 * self.pixel_delta_v);
+        let pixel_sample = pixel_center + sample_square;
+        let ray_direction = pixel_sample - self.center;
+        Ray {
+            origin: self.center,
+            direction: ray_direction,
+        }
+    }
+    pub fn render_picture<T>(&self, scene: T, version: &str) -> io::Result<()>
+    where
+        T: Hittable,
+    {
+        let mut pixels = String::new();
+        for j in 0..self.image_height {
+            for i in 0..self.image_width {
+                let pixel_color = self.get_ray(i, j).color(&scene) * 255.;
+                pixels += &format!(
+                    "{} {} {} \n",
+                    pixel_color.x.round() as u16,
+                    pixel_color.y.round() as u16,
+                    pixel_color.z.round() as u16
+                );
+            }
+        }
+        fs::write(
+            format!("images/output{}.ppm", version),
+            format!(
+                "P3
+{} {}
+{}
+{pixels}
+",
+                self.image_width, self.image_height, self.max_value
+            ),
+        )?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
+use camera::*;
 use glam::{f64, DVec3};
-use structs::Ray;
 use structs::*;
+mod camera;
 mod structs;
 use std::{fs, io};
-const MAX_VALUE: u8 = 255;
+const MAX_VALUE: u16 = 255;
 const ASPECT_RATIO: f64 = 16. / 9.;
 const IMAGE_WIDTH: u32 = 400;
-const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
-const VIEWPORT_HEIGHT: f64 = 2.0;
-const VIEWPORT_WIDTH: f64 = VIEWPORT_HEIGHT * (IMAGE_WIDTH as f64 / IMAGE_HEIGHT as f64);
-const FOCAL_LENGTH: f64 = 1.0;
-const CAMERA_CENTER: DVec3 = DVec3::ZERO;
-const VIEWPORT_U: DVec3 = DVec3::new(VIEWPORT_WIDTH, 0., 0.);
-const VIEWPORT_V: DVec3 = DVec3::new(0., -VIEWPORT_HEIGHT, 0.);
+
 fn main() -> io::Result<()> {
+    let camera: Camera = Camera::new(IMAGE_WIDTH, ASPECT_RATIO, MAX_VALUE);
     let mut scene = HittableList {
         elements: Vec::new(),
     };
@@ -26,42 +22,7 @@ fn main() -> io::Result<()> {
         center: DVec3::new(0., -100.5, -10.),
         radius: 100.,
     });
-
-    let pixel_delta_u: DVec3 = VIEWPORT_U / IMAGE_WIDTH as f64;
-    let pixel_delta_v: DVec3 = VIEWPORT_V / IMAGE_HEIGHT as f64;
-    let viewport_upper_left: DVec3 =
-        CAMERA_CENTER - DVec3::new(0., 0., FOCAL_LENGTH) - VIEWPORT_U / 2. - VIEWPORT_V / 2.;
-    let pixel00_loc: DVec3 = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-    let mut pixels = String::new();
-    for j in 0..IMAGE_HEIGHT {
-        for i in 0..IMAGE_WIDTH {
-            let pixel_center =
-                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
-            let ray_direction = pixel_center - CAMERA_CENTER;
-            let ray = Ray {
-                origin: CAMERA_CENTER,
-                direction: ray_direction,
-            };
-            let pixel_color = ray.color(&scene) * 255.;
-            pixels += &format!(
-                "{} {} {} \n",
-                pixel_color.x.round() as u16,
-                pixel_color.y.round() as u16,
-                pixel_color.z.round() as u16
-            );
-        }
-    }
-    fs::write(
-        "output2.ppm",
-        format!(
-            "P3
-{IMAGE_WIDTH} {IMAGE_HEIGHT}
-{MAX_VALUE}
-{pixels}
-"
-        ),
-    )?;
-
+    camera.render_picture(scene, "3")?;
     Ok(())
 }
 //used earlier
